21526fc749c3fdadaab4be090e22d4738499c77e
andrei_gubsky <anrei.gubsky@gmail.com>
2026-01-12 22:51:02 +0200

    Fix app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,11 @@
 // src/App.tsx
 import css from "./App.module.css";
 import { useState } from "react";
-import type { Votes, VoteType } from "../types/votes";
-import CafeInfo from "./CafeInfo/CafeInfo";
-import VoteOptions from "./VoteOptions/VoteOptions";
-import VoteStats from "./VoteStats/VoteStats";
-import Notification from "./Notification/Notification";
+import type { Votes, VoteType } from "../../types/votes";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
 
 export default function App() {
   /**
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import './index.css'
-import App from "./components/App.tsx";
-import "./components/App.module.css";
+import App from "./components/App/App.tsx";
+import "./components/App/App.module.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
